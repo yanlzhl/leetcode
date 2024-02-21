faba67fec8b397c070fcf57c78a9b774c0465ac8
yanlzhl <yanlingzhi.me@gmail.com>
2024-02-21 18:29:31 +1100

    83 remove-duplicates-from-sorted-list

--- src/com/yanlz/algorith/array/RemoveDuplicates_array_26.java
+++ src/com/yanlz/algorith/array/RemoveDuplicates_array_26.java
@@ -1,11 +1,32 @@
 package com.yanlz.algorith.array;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
+/**
+ * remove-duplicates
+ *
+ * @author: Yan
+ * @createTime: 2024/02/21 17:32
+ * @description:
+ * 26: https://leetcode.cn/problems/remove-duplicates-from-sorted-array/
+ * 83: https://leetcode.cn/problems/remove-duplicates-from-sorted-list/description/
+ */
 public class RemoveDuplicates_array_26 {
     public static void main(String[] args) {
+
+        // entrance of 26
         int[] removeDuplicatesArray = new int[]{1,2,3,3,3,4,5};
-        System.out.println(removeDuplicates(removeDuplicatesArray));
+        //System.out.println(removeDuplicates(removeDuplicatesArray));
+
+        // entrance of 83
+        ListNode nodeFive = new ListNode(3,new ListNode());
+        ListNode nodeFour = new ListNode(3,nodeFive);
+        ListNode nodeThree = new ListNode(2,nodeFour);
+        ListNode nodeSecond = new ListNode(1,nodeThree);
+        ListNode nodeHead = new ListNode(1,nodeSecond);
+        System.out.println(deleteDuplicates(nodeHead));
     }
 
     /**
@@ -40,4 +61,54 @@ public class RemoveDuplicates_array_26 {
 
         return slow + 1;
     }
+
+    public static ListNode deleteDuplicates(ListNode listNode) {
+        if (listNode == null || listNode.next == null){
+            return null;
+        }
+
+        ListNode slow = listNode;
+        ListNode fast = listNode.next;
+
+        while (fast.next != null){
+            if (fast.val != slow.val){
+                //将重复元素对下一个数据向后移动覆盖前一个重复对
+                slow.next = fast;
+                //慢指针向前一步，放在上一行代码后才可衔接上，这一步容易忘（以为移动了）。
+                slow = slow.next;
+            }
+            fast = fast.next;
+        }
+
+        slow.next = null;
+
+        ListNode node = listNode;
+        List<ListNode> list = new ArrayList<>();
+        list.add(node);
+        while (node.next !=null){
+            node = node.next;
+            list.add(node);
+        }
+        for (ListNode item : list){
+            System.out.println(item.val);
+        }
+
+
+        return listNode;
+    }
 }
+
+
+class ListNode {
+  int val;
+  ListNode next;
+  ListNode() {}
+  ListNode(int val) {
+      this.val = val;
+  }
+  ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+  }
+}
+
