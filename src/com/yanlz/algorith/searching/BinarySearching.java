fe1afba3d6fc366f42b52691735ecb6df76eca47
yanlzhl <yanlingzhi.me@gmail.com>
2024-02-26 01:02:51 +1100

    5 longest-palindromic-substring

--- src/com/yanlz/algorith/searching/BinarySearching.java
+++ src/com/yanlz/algorith/searching/BinarySearching.java
@@ -22,7 +22,10 @@ public class BinarySearching {
 
         //344
 //        reverseArray(new int[]{2,7,11,16});
-        reverseArray(new char[]{'a','b','c','d'});
+//        reverseArray(new char[]{'a','b','c','d'});
+
+        //5 cabcbad eabaabaf
+        System.out.println(findTheLongestPalindrome("eabaabaf"));;
     }
 
     /**
@@ -131,10 +134,48 @@ public class BinarySearching {
             left++;
             right--;
         }
-
         return true;
     }
 
+    /**
+     * 找到符合回文串--再外层再做比较
+     * left right再奇数字符串中起始点是一样的；abcba
+     * left right偶数串中，起始点不重合，right 再left右边1位  abaaba
+     * @param string
+     * @param left 左侧开始出，递减向左移动
+     * @param right 右侧开始出，递减向右移动
+     * @return
+     */
+    private static String findThePalindrome(String string,int left, int right){
+        while (left>=0 && right < string.length() && (string.charAt(left) == string.charAt(right))){
+            left--;
+            right++;
+        }
+
+        //此处需要特别注意，因为left--，right++运算在先，
+        // 而substring中，the beginning index, inclusive，the ending index, exclusive.
+        //因此left+1，right无需要再+1了
+        return string.substring(left+1,right);
+    }
+
+    /**
+     *  5 https://leetcode.cn/problems/longest-palindromic-substring/description/
+     * @param string Palindrome
+     * @return
+     */
+    public  static String findTheLongestPalindrome(String string){
+        String longestSubPalindrome = "";
+        for (int i = 0; i < string.length(); i++) {
+            String oddStr = findThePalindrome(string,i,i);
+            String evenStr = findThePalindrome(string,i,i+1);
+
+            //本轮最长的
+            String tempLongest = oddStr.length() > evenStr.length() ? oddStr:evenStr;
+            //再和上一轮最长的比
+            longestSubPalindrome = tempLongest.length() > longestSubPalindrome.length() ?  tempLongest:longestSubPalindrome;
+        }
+        return longestSubPalindrome;
+    }
 
 
 }
