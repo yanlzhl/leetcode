4ba593414ffd9e633af691b2fd6c23f325462996
yanlzhl <yanlingzhi.me@gmail.com>
2024-11-25 12:26:24 +1100

    update: ..

--- src/com/yanlz/algorith/linkedList/LinkedListOperation.java
+++ src/com/yanlz/algorith/linkedList/LinkedListOperation.java
@@ -2,6 +2,8 @@ package com.yanlz.algorith.linkedList;
 
 /**
  *
+ * 21 合并两个有序链表
+ *
  * @author: Yan
  * @createTime: 2024/04/09 10:58
  * @description:
@@ -20,17 +22,19 @@ public class LinkedListOperation {
         ListNode nodeSecond2 = new ListNode(2,nodeThree2);
         ListNode nodeList2 = new ListNode(1,nodeSecond2);
 
-        ListNode result = mergeLindedLists(nodeList1,nodeList2);
+//        ListNode result = mergeLindedLists(nodeList1,nodeList2);
 
-//        for (ListNode node = result; result.next != null;  node = node.next){
-//            System.out.println(node.val);
-//        }
+        ListNode result = reverse(nodeList1);
         while (result!=null){
             if (result.val != 0){ //处理没有值的节点，因为默认是0
                 System.out.println(result.val);
             }
             result = result.next;
         }
+
+
+
+
 
     }
 
@@ -67,4 +71,41 @@ public class LinkedListOperation {
 
         return dummy.next;
     }
+
+    static ListNode partition(ListNode head, int x) {
+        ListNode dummy = new ListNode(-1), p1 = dummy;
+        ListNode  p2 = dummy;
+
+        while (head != null){
+            if (head.val > x){
+                p2.next = head;
+                p2 = p2.next;
+            }else {
+                p1.next = head;
+                p1 = p1.next;
+            }
+            head = head.next;
+
+            // 注意：不能直接让 p 往后走，否则链表中会出现环，也就是原链表倒数第二个节点会指向倒数第一个（且倒数2个节点拆分在P1和P2上）
+            ListNode temp = head.next;
+            temp.next= null;
+            head = temp;
+        }
+
+        p1.next = p2.next;
+        return dummy.next;
+    }
+
+    //  206 递归反转整个链表 https://leetcode.cn/problems/reverse-linked-list/
+    // 定义：输入一个单链表头结点，将该链表反转，返回新的头结点  1 5 6 7 8
+    static ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode last = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
+    }
+
 }
